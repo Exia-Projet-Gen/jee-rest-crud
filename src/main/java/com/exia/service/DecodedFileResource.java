56b1a5d95df9c5e70fcede4b07e0c53a7c8b7f49
HyacintheK <hyacinthe.knobloch@viacesi.fr>
2017-06-29 16:06:56 +0200

    update getfiles

--- src/main/java/com/exia/service/DecodedFileResource.java
+++ src/main/java/com/exia/service/DecodedFileResource.java
@@ -5,16 +5,20 @@
  */
 package com.exia.service;
 
+import com.exia.domain.JAXFile;
 import java.io.StringReader;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -76,14 +80,24 @@ public class DecodedFileResource {
         }
         
         Boolean isValid = decodedFileService.saveValidMessage(decodedText, keyValue, fileName, matchPercent, mailAddress);
-                
-        
+                       
         Response resp;
         if (isValid) {
             resp = Response.accepted().build();
         } else {
             resp = Response.status(400).entity("A problem occured while deleting the word in database.").build();
         }
+        return resp;
+    } 
+
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        List<JAXFile> words = decodedFileService.getDecodedFiles();
+        GenericEntity<List<JAXFile>> genericList = new GenericEntity<List<JAXFile>>(words){};
+        
+        Response resp = Response.ok(genericList).build();
         return resp;
     } 
 }
